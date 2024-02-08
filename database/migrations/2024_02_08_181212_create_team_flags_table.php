04961a47ed8540b78e54110170cfe5c4bd52d581
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-02-08 20:12:35 +0200

    add flag

--- database/migrations/2024_02_08_181212_create_team_flags_table.php
+++ database/migrations/2024_02_08_181212_create_team_flags_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('flags', function (Blueprint $table) {
+        Schema::create('team_flags', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('team_id')->unique();
-            $table->string('flag_url');
+            $table->string('flag_url')->nullable();
+            $table->string('flag_name');
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('flags');
+        Schema::dropIfExists('team_flags');
     }
 };
