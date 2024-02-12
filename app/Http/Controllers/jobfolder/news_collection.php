945e216a05c27ddac1f5cfbf8bb99286b76fa079
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-02-12 18:00:40 +0200

    fix bugs and test feed url

--- app/Http/Controllers/jobfolder/news_collection.php
+++ app/Http/Controllers/jobfolder/news_collection.php
@@ -17,36 +17,41 @@ class news_collection extends Controller
         $sources_arr = source::get();
 
         foreach ($sources_arr as $source) {
-            // Define the XML file URL
-            $f = FeedReader::read($source->url);
 
-            foreach ($f->get_items(0, $f->get_item_quantity()) as $item) {
+            if ($source->url == 'https://www.youm7.com/') {
+                
+            } else {
+                // Define the XML file URL
+                $f = FeedReader::read($source->url);
 
-                // Insert the data into the database
-                $dataSource = [
-                    'source_id' => $source->id,
-                    'publish_date' => $item->get_date(),
-                ];
-                $Artical = Article::create($dataSource);
+                foreach ($f->get_items(0, $f->get_item_quantity()) as $item) {
+                    // Insert the data into the database
+                    $dataSource = [
+                        'source_id' => $source->id,
+                        'publish_date' => $item->get_date(),
+                    ];
+                    $Artical = Article::create($dataSource);
 
-                // Store the element attributes in an associative array
-                $data = [
-                    'article_id' => $Artical->id,
-                    'title' => $item->get_title() ?? $item->get_author(),
-                    'description' => $item->get_description(),
-                    'link' => $item->get_link(),
-                    'image' => $item->get_permalink(),
-                    'video' => $item->get_thumbnail(),
-                ];
-                // Insert the data into the database
-                DB::table('article_contents')->insert($data);
+                    // Store the element attributes in an associative array
+                    $data = [
+                        'article_id' => $Artical->id,
+                        'title' => $item->get_title() ?? $item->get_author(),
+                        'description' => $item->get_description(),
+                        'link' => $item->get_link(),
+                        'image' => $item->get_permalink(),
+                        'video' => $item->get_thumbnail(),
+                    ];
+                    // Insert the data into the database
+                    DB::table('article_contents')->insert($data);
 
-                ArticleCategory::create([
-                    'article_id' => $Artical->id,
-                    'category_id' => $source->category_id,
-                    'subcategory_id' => $source->subcategory_id,
-                ]);
+                    ArticleCategory::create([
+                        'article_id' => $Artical->id,
+                        'category_id' => $source->category_id,
+                        'subcategory_id' => $source->subcategory_id,
+                    ]);
+                }
             }
+
         }
         return response()->json('suceess', 200);
     }
